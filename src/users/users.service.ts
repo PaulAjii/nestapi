94e6a1b67669adcba0f6db19fce421c2cbef26bf
PaulAjii <paulajii134@gmail.com>
2025-03-02 00:38:03 +0100

    fix: handled empty return bugs during fetching

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
@@ -69,7 +69,11 @@ export class UsersService {
       
     findAll(role?: "INTERN" | "ENGINEER" | "ADMIN") {
         if (role) {
-            return this.users.filter(user => user.role === role)
+            const rolesArray = this.users.filter(user => user.role === role)
+
+            if (rolesArray.length === 0) throw new NotFoundException("User role not found")
+
+            return rolesArray
         }
 
         return this.users
@@ -77,6 +81,8 @@ export class UsersService {
 
     findOne(id: number) {
         const user = this.users.find(user => user.id === id)
+
+        if (!user) throw new NotFoundException("User not found")
 
         return user
     }
