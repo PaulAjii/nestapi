cc2e2f9dbcf37e86a1fbb38d3726445f64f91699
PaulAjii <paulajii134@gmail.com>
2025-03-02 00:01:03 +0100

    refactor: used the dto type validation is user service and controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -16,13 +18,13 @@ export class UsersController {
     }
 
     @Post()
-    create(@Body() user: {name: string, email: string, role: "INTERN" | "ENGINEER" | "ADMIN"}) {
-        return this.usersService.create(user)
+    create(@Body() createUserDto: CreateUserDto) {
+        return this.usersService.create(createUserDto)
     }
 
     @Patch(':id')
-    update(@Param('id', ParseIntPipe) id: number, @Body() updatedUser: {name?: string, email?: string, role?: "INTERN" | "ENGINEER" | "ADMIN"}) {
-        return this.usersService.update(id, updatedUser)
+    update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
+        return this.usersService.update(id, updateUserDto)
     }
 
     @Delete(':id')
